1fe63fef49b92197662e3e22cfa16c497e224174
tekla <tekla.gogua.2006@gmail.com>
2022-05-24 09:14:11 +0400

    added react file

---
+++ react-demo/src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>well... It's working</div>
+    </div>
+  );
+}
+
+export default App;
